7ec7586e14c40b6850217de1e449dddc9afcdc08
sinhvien <sinhvien>
2021-04-08 10:01:50 +0700

    MVC lab 4

---
+++ hailongqng/hailongqng/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="hailongqng.MvcApplication" Language="C#" %>
---
+++ hailongqng/hailongqng/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hailongqng.Startup))]
+namespace hailongqng
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
